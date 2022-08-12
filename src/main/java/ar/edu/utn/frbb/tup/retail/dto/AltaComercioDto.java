36e222ea32e950fc90a9b16b0cfa5e69965298e8
Juan Cruz Wehitt <juan.cruz.wehitt@gmail.com>
2022-08-12 09:31:15 -0300

    Implementacion de Dto - AltaComercioDto y AltaProductoDto

--- src/main/java/ar/edu/utn/frbb/tup/retail/dto/AltaComercioDto.java
+++ src/main/java/ar/edu/utn/frbb/tup/retail/dto/AltaComercioDto.java
@@ -1,4 +1,38 @@
 package ar.edu.utn.frbb.tup.retail.dto;
 
 public class AltaComercioDto {
+    private String nombre;
+    private String cuit;
+
+    private String direccion;
+
+    public AltaComercioDto(String nombre, String cuit, String direccion) {
+        this.nombre = nombre;
+        this.cuit = cuit;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(String cuit) {
+        this.cuit = cuit;
+    }
 }
