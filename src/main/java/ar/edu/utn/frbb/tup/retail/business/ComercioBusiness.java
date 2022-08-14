4aa1738a9542d813a4ef29ac62b94b6935721794
Juan Cruz Wehitt <juan.cruz.wehitt@gmail.com>
2022-08-14 00:42:17 -0300

    Descarte de clase para entidad Comercio.

--- src/main/java/ar/edu/utn/frbb/tup/retail/business/ComercioBusiness.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.utn.frbb.tup.retail.business;
-
-import ar.edu.utn.frbb.tup.retail.dto.AltaComercioDto;
-import ar.edu.utn.frbb.tup.retail.model.Comercio;
-
-public interface ComercioBusiness {
-    Comercio altaComercio(AltaComercioDto dto);
-}
